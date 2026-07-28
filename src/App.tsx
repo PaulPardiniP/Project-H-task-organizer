b3bfa957fcc85ed84d943114574ca94d2e73e1c4
PaulPardiniP <paulpardini.it@gmail.com>
2026-07-28 17:55:17 +0200

    Rutas protegidas

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,28 @@
-import { auth, db } from "./services/firebase";
+import { Routes, Route, Navigate } from "react-router-dom";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+import RequireAuth from "./routes/RequireAuth";
+
+function DashboardPage() {
+  return <h2>Dashboard (acá van a ir las tareas)</h2>;
+}
 
 function App() {
-  console.log("Auth:", auth);
-  console.log("DB:", db);
-  return <div>MateCode</div>;
+  return (
+    <Routes>
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/register" element={<RegisterPage />} />
+      <Route
+        path="/dashboard"
+        element={
+          <RequireAuth>
+            <DashboardPage />
+          </RequireAuth>
+        }
+      />
+      <Route path="/" element={<Navigate to="/dashboard" replace />} />
+    </Routes>
+  );
 }
 
 export default App;
---
+++ src/routes/RequireAuth.tsx
@@ -0,0 +1,20 @@
+// src/routes/RequireAuth.tsx
+import { useAuth } from "../features/auth/authenticator";
+import { Navigate, useLocation } from "react-router-dom";
+import type { ReactNode, JSX } from "react";
+
+interface RequireAuthProps {
+  children: ReactNode;
+}
+
+function RequireAuth({ children }: RequireAuthProps): JSX.Element {
+  const { user, loading } = useAuth();
+  const location = useLocation();
+
+  if (loading) return <p>Cargando sesión...</p>;
+  if (!user) return <Navigate to="/login" state={{ from: location }} replace />;
+
+  return <>{children}</>;
+}
+
+export default RequireAuth;
